f6fc3b3b74806040f73c3d32275ebd288fbc4ef8
crmwhatsapp <timecomercial.eucirlenealves@gmail.com>
2026-06-26 03:27:39 -0400

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return {
+      beforeFiles: [
+        { source: "/.well-known/oauth-authorization-server", destination: "/wkas" },
+        { source: "/.well-known/oauth-protected-resource", destination: "/wkpr" },
+      ],
+    };
+  },
+};
 module.exports = nextConfig;
